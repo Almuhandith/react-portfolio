870d8f91cbb19d216e3bf632732a03928585fede
Almuhandith <almuhandith1497@gmail.com>
2023-06-05 18:32:52 +0100

    components

--- src/app/page.js
+++ src/app/page.js
@@ -43,6 +43,13 @@ export default function Home() {
                 </a>
           </div>
         </section>
+        <footer className='flex text-xs pb-8 justify-center items-center text-slate-300 hover:text-cyan-200
+        '>
+          <div>
+            <p>Designed and Developed by <strong className='dark:text-slate-200'>Abdulsamad Raji</strong>. </p>
+            <p>Built with <strong className='dark:text-slate-200'>Next.js</strong> and <strong className='dark:text-slate-200'>Tailwind CSS</strong>, deployed with <strong className='dark:text-slate-200'>Vercel</strong>.</p>
+          </div>
+        </footer>
         <Cursor />
       </main>
     </div>
