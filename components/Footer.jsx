1f01969714ee08145252201c2f31ccc6c907d28e
Almuhandith <almuhandith1497@gmail.com>
2023-07-30 08:45:55 +0100

    navbar fix

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='flex text-xs pb-8 justify-center items-center text-slate-300 hover:text-cyan-200
-    '>
+    <footer className='flex text-xs pb-8 justify-center items-center text-slate-300 hover:text-cyan-200'>
       <div>
         <p>Designed and Developed by <strong className='dark:text-slate-200'>Abdulsamad Raji</strong>. </p>
         <p>Built with <strong className='dark:text-slate-200'>Next.js</strong> and <strong className='dark:text-slate-200'>Tailwind CSS</strong>, deployed with <strong className='dark:text-slate-200'>Vercel</strong>.</p>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -31,7 +31,7 @@ function Navbar() {
 
   return (
     <main>
-        <nav  className={`w-full border-b drop-shadow-2xl fixed top-0 left-0 right-0 z-50`}>
+        <nav  className={`w-full border-b bg-white dark:bg-primary fixed top-0 left-0 right-0 z-50`}>
           <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
             <div className="flex items-center justify-between py-3 md:py-6 md:block">
               <Link href="/">
